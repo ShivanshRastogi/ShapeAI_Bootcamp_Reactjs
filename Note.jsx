25670276c5ada9eaff684b6cd9ef6ef1264db0e4
ShivanshRastogi <72811522+ShivanshRastogi@users.noreply.github.com>
2021-06-05 19:26:59 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <h1>Web Development with Javascript & React.js</h1>
+      <p>
+        This is an amazing bootcamp by ShapeAI. The instructor name is Shaurya
+        Sinha. He has covered basics of HTML, Javascript & React.js
+      </p>
+    </div>
+  );
+}
+
+export default Note;
